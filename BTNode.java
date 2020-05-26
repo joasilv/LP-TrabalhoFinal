377725d810a28a36af6fee1f5a515799ced5e2fa
joasilv <150118001@esg.ipsantarem.pt>
2020-05-26 18:59:31 +0100

    Update BTNode.java

--- BTNode.java
+++ BTNode.java
@@ -61,6 +61,9 @@ public class BTNode<E> {
         }
     }
 
+    /**
+     * 
+     */
     public void inorderPrint(){
         if(left !=null){
             left.inorderPrint();
@@ -70,4 +73,112 @@ public class BTNode<E> {
             right.inorderPrint();
         }
     }
+/**
+ * 
+ */
+
+    public boolean isLeaf(){
+        return (left == null ) && (right == null);
+    }
+
+/**
+ * 
+ */
+    public void preorderPrint(){
+        System.out.println(data);
+        if(left != null){
+            left.preorderPrint();
+        }
+        if(right != null){
+            right.preorderPrint();
+        }
+    }
+
+
+    /**
+     * 
+     */
+    public void postorderPrint(){
+        if(left!=null){
+            left.postorderPrint();
+        }
+        if(right!=null){
+            right.postorderPrint();
+        }
+    }
+
+    /**
+     * 
+     * 
+     */
+
+     public void print(int depth){
+         int i;
+         //
+         for(i = 1; i <= depth; i++){
+             System.out.println("    ");
+         }
+         System.out.println(data);
+
+         //Imprime a subtree esquerda
+         if(left !=null){
+             left.print(depth+1);
+         }
+         else if(right != null){
+             for(i=1; i<= depth+1;i++){
+                 System.out.println("   ");
+             }
+             System.out.println("--");
+         }
+     }
+
+     /**
+      * 
+      */
+
+      public BTNode<E> removeLeftmost(){
+          if(left ==null){
+              return right;
+          }
+          else{
+              left= left.removeLeftmost();
+              return this;
+          }
+      }
+
+
+      /**
+       * 
+       */
+
+       public BTNode<E> removeRightmost(){
+           if(right== null){
+               return left;
+           }
+           else{
+               right=right.removeRightmost();
+               return this;
+           }
+       }
+
+       /**
+        * 
+
+        */
+
+        public void setData(E newData){
+            data=newData;
+        }
+
+        /**
+         * 
+         * 
+         */
+
+         public setLeft(BTNode<E> newLeft){
+             left = newLeft;
+         }
+         public setRight(BTNode<E> newRight){
+             right= newRight;
+         }
 }
