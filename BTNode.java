bab321cf2bb38737185f0b4748fd94b9057365c6
joasilv <150118001@esg.ipsantarem.pt>
2020-05-26 18:16:46 +0100

    Update BTNode.java

--- BTNode.java
+++ BTNode.java
@@ -19,10 +19,55 @@ public class BTNode<E> {
     }
 
     /**
-     * 
-     * @return
+     * Acessor para obter uma referência ao filho esquerdo deste nó.
+     * @return uma referência ao filho esquerdo deste nó (ou a referência nula, se houver não é filho esquerdo)
      */
     public BTNode <E> getLeft(){
         return left;
+    }
+
+    /**
+     * Acessor para obter os dados do nó mais à esquerda da árvore abaixo 
+     * este nó.
+     * @return os dados do nó mais profundo que podem ser alcançados a partir desse nó
+   * seguindo os links à esquerda.
+     */
+    public E getLeftmostData(){
+        if(left == null){
+            return data;
+        }
+        else{
+            return left.getLeftmostData();        }
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public BTNode<E> getRight(){
+        return right;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public E getRightmostData(){
+        if(left==null){
+            return data;
+        }
+        else{
+            return left.getRightmostData();
+        }
+    }
+
+    public void inorderPrint(){
+        if(left !=null){
+            left.inorderPrint();
+        }
+        System.out.println(data);
+        if(right !=null){
+            right.inorderPrint();
+        }
     }
 }
