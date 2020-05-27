090061067005c8d16a77bb34692c4e074dd3870e
joasilv <150118001@esg.ipsantarem.pt>
2020-05-27 15:56:21 +0100

    Create bot.java

---
+++ bot.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/**
+ * Esta classe cria um "Bot" que opera como a voz e o programa do programa.
+ * Ele cria uma nova Árvore Binária e a manipula usando métodos pertencentes à classe BinaryTree.
+ * Este "Bot" fará as perguntas e inserirá as respostas em uma Árvore Binária.
+ * Ele continuará tentando adivinhar até que desista ou alcance uma resposta correta.
+ *
+ */
+
+public class bot {
+    
+}
